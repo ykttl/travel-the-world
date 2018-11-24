6eb0acc14e62f364d8a0cd18a5a64f271ea513e4
Yuko <yuko.mazurek@gmail.com>
2018-11-24 09:43:48 -0800

    Updates

---
+++ precache-manifest.22bc0d9d881b1b0633df468136001fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "408aeda29759723a003d",
+    "url": "/travel-the-world/static/js/runtime~main.408aeda2.js"
+  },
+  {
+    "revision": "f3dd92348374fb7f8165",
+    "url": "/travel-the-world/static/js/main.f3dd9234.chunk.js"
+  },
+  {
+    "revision": "c9c111bde3f2f46ff411",
+    "url": "/travel-the-world/static/js/1.c9c111bd.chunk.js"
+  },
+  {
+    "revision": "f3dd92348374fb7f8165",
+    "url": "/travel-the-world/static/css/main.f92de1a0.chunk.css"
+  },
+  {
+    "revision": "95ca8d54c7c0bccb254638acb2a155d5",
+    "url": "/travel-the-world/index.html"
+  }
+];
